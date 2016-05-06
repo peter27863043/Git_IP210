7d14d4d3c4ee26fa511facc99e6d63a9f69cffda
peterlin <peter27863043@yahoo.com>
2016-05-06 11:50:46 +0800

    2016/05/06 7th commit

--- FullSource/src_code/include/option.h
+++ FullSource/src_code/include/option.h
@@ -1,5 +1,5 @@
 //1.Version
-#define WEB_CODE_VERSION "V1.44.4 2016_02_22"
+#define WEB_CODE_VERSION "V1.44.4 2016_05_06"
 #define WEB_PAGE_VERSION "V1.0.0"
 #define TOP_BANNER "=================================================="
 #define BTN_BANNER TOP_BANNER
