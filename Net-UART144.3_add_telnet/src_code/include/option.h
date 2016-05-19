935faa2ae7d0463bc4315beaf22684749b6973d6
peterlin <peter27863043@yahoo.com>
2016-05-19 17:32:36 +0800

    Default IP: 169.254.81.38

--- Net-UART144.3_add_telnet/src_code/include/option.h
+++ Net-UART144.3_add_telnet/src_code/include/option.h
@@ -1,5 +1,5 @@
 //1.Version
-#define WEB_CODE_VERSION "V1.44.3 2016/05/10"
+#define WEB_CODE_VERSION "V1.44.3 2016/05/19"
 #define WEB_PAGE_VERSION "V1.0.0"
 #define TOP_BANNER "=================================================="
 #define BTN_BANNER TOP_BANNER
